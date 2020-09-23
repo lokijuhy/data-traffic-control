d5fb2df2a150a1a98cf1deee5a8ca34c750fe59e
lokijuhy <laura.kinkead@uzh.ch>
2020-09-23 16:47:15 +0200

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='datatc',
-      version='0.0.2',
+      version='0.0.3',
       description='',
       url='https://github.com/uzh-dqbm-cmi/data-traffic-control',
       packages=['datatc', ],
